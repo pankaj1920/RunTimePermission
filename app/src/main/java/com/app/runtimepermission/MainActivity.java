44c9e16f8928dbe987c6317a1abe25936a12cc04
pankaj1920 <42286224+pankaj1920@users.noreply.github.com>
2019-08-03 13:55:59 +0530

    RunTime Permission by Harsha Sir

--- app/src/main/java/com/app/runtimepermission/MainActivity.java
+++ app/src/main/java/com/app/runtimepermission/MainActivity.java
@@ -2,6 +2,7 @@ package com.app.runtimepermission;
 
 import android.Manifest;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -31,61 +32,64 @@ public class MainActivity extends AppCompatActivity {
                 //but first we have to check if permission is already granted
                 if (ContextCompat.checkSelfPermission(MainActivity.this
                         , Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(MainActivity.this, "Permission is Granted", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "Permission already Granted", Toast.LENGTH_SHORT).show();
                 } else {
+                    // if the permission is not granted yet. we want to request this permission
                     requestContactPermission();
                 }
             }
         });
 
+
       Button  externl_permission = findViewById(R.id.externl_permission);
         externl_permission.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //when user click on this button we want to request the permission
-                //but first we have to check if permission is already granted
-                if (ContextCompat.checkSelfPermission(MainActivity.this,
-                        Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(MainActivity.this, "Permission already Granted", Toast.LENGTH_SHORT).show();
-                } else {
-                    // if the permission is not granted yet. we want to request this permission
-                    requestStoragePermission();
-                }
+                storagePermisiom();
             }
         });
     }
 
-    private void requestStoragePermission() {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
-            // This method will show user a dialog which explain why we need this permission
-            //this happen when user already denied the permission before and tries to acess it again
-
-            new AlertDialog.Builder(MainActivity.this)
-                    .setTitle("Permision needed")
-                    .setMessage("This permision is needed for this and that")
-                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
-
-                        }
-                    })
-                    .setNegativeButton("cancle", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            //when this cancle butoon is clicke we need to dismiss the dialog
-                            dialog.dismiss();
-                        }
-                    }).create().show();
+    //This is From Harsha Sir
+    private void storagePermisiom() {
+        if (askForPermission(Manifest.permission.READ_EXTERNAL_STORAGE, 1)) {
+            Intent intent = new Intent(MainActivity.this,WelcomeActivity.class);
+            startActivity(intent);
         } else {
-            //Request permission
-            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+            displayPermission(Manifest.permission.READ_EXTERNAL_STORAGE, 1);
+        }
+    }
 
+    private boolean askForPermission(String permission, Integer requestCode) {
+        if (ContextCompat.checkSelfPermission(MainActivity.this, permission)
+                != PackageManager.PERMISSION_GRANTED) {
+            return false;
+        } else {
+            Toast.makeText(this, "" + permission + " is already granted.", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+    }
+
+    void displayPermission(String permission, Integer requestCode) {
+        // Should we show an explanation?
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                MainActivity.this, permission)) {
+
+            //This is called if user has denied the permission before
+            //In this case I am just asking the permission again
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{permission}, requestCode);
+
+        } else {
+
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{permission}, requestCode);
         }
     }
 
     //Request Call Permission
 
+    // This is From Coding With Flow
     private void requestContactPermission() {
         if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.READ_CONTACTS)) {
             // This method will show user a dialog which explain why we need this permission
@@ -123,9 +127,9 @@ public class MainActivity extends AppCompatActivity {
 
         if (ActivityCompat.checkSelfPermission(this, permissions[0]) == PackageManager.PERMISSION_GRANTED) {
             if (requestCode == 1) {
-                requestStoragePermission();
+                storagePermisiom();
             } else if (requestCode == 2) {
-                requestStoragePermission();
+                requestContactPermission();
             }
         }
     }
